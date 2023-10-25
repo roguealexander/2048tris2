8c69d872c455cb6942356048a6637e86a77c9574
Rogue Alexander <rotkosky.rogue@gmail.com>
2023-10-25 13:58:01 +0200

    Top out view

--- packages/app/components/hold.tsx
+++ packages/app/components/hold.tsx
@@ -5,13 +5,15 @@ import { Shake, SizableText, XStack } from '@my/ui'
 
 export const Hold = observer(() => {
   return (
-    <Shake shakeKey={`${state$.holdShakeCount.get()}`} shakeDistance={12} shakeTimes={3}>
+    <>
       <SizableText>Hold:</SizableText>
       <XStack w="$12" h="$12" jc="center" ai="center" bg="$playarea" bw={4} boc="$border">
-        <XStack o={state$.holdAvailable.get() ? 1 : 0.5}>
-          <Tile size={state$.heldTile} />
-        </XStack>
+        <Shake shakeKey={`${state$.holdShakeCount.get()}`} shakeDistance={12} shakeTimes={3}>
+          <XStack o={state$.holdAvailable.get() ? 1 : 0.5}>
+            <Tile size={state$.heldTile} />
+          </XStack>
+        </Shake>
       </XStack>
-    </Shake>
+    </>
   )
 })
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,8 +1,6 @@
 import { observable } from '@legendapp/state'
-import {
-  XStack,
-  YStack,
-} from '@my/ui'
+import { observer } from '@legendapp/state/react'
+import { XStack, YStack } from '@my/ui'
 import { ActiveTilesHistogram } from 'app/components/active-tile-histogram'
 import { Board } from 'app/components/board'
 import { Hold } from 'app/components/hold'
@@ -10,22 +8,35 @@ import { HoldListener } from 'app/components/hold-listener'
 import { Queue } from 'app/components/queue'
 import { Stats } from 'app/components/stats'
 import { Tile } from 'app/components/tile'
-import { TopOutOverlay } from 'app/components/top-out-overlay'
+import { TopOutLeftPanel } from 'app/components/top-out-overlay'
+import { state$ } from 'app/state'
 import { TileSize } from 'app/types'
 import React from 'react'
+
+const ActiveLeftPanel = observer(() => {
+  if (state$.toppedOut.get()) return null
+  return (
+    <YStack w="$12" gap="$2">
+      <Hold />
+      <Stats />
+      <ActiveTilesHistogram />
+    </YStack>
+  )
+})
+
+const ActiveRightPanel = observer(() => {
+  if (state$.toppedOut.get()) return null
+  return <Queue />
+})
 
 export function HomeScreen() {
   return (
     <XStack maw={1480} als="center" f={1} gap={64} pt={64}>
       <HoldListener />
-      <YStack w="$12" gap="$2">
-        <Hold />
-        <Stats />
-        <ActiveTilesHistogram />
-      </YStack>
+      <TopOutLeftPanel />
+      <ActiveLeftPanel />
       <Board />
-      <Queue />
-      <TopOutOverlay />
+      <ActiveRightPanel />
     </XStack>
   )
 }
