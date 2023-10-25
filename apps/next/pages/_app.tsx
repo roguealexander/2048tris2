9c648ef96370e7b1bff4ac82533a35355b2e466a
Rogue Alexander <rotkosky.rogue@gmail.com>
2023-10-25 16:20:58 +0200

    Test release

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -36,7 +36,7 @@ function MyApp({
         <meta name="description" content="Tamagui Universal Starter" />
         <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
       </Head>
       <NextThemeProvider
